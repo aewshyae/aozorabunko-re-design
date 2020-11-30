1511a1b49bd92fa4f762f66510a52845e1282a4f
aewshyae <1528058+aewshyae@users.noreply.github.com>
2020-11-30 17:44:39 +0900

    update nuxt.config: remove 'mode', add 'target'

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,15 @@
 import axios from 'axios'
-require('dotenv').config()
 
 export default {
-  mode: 'universal',
+  target: 'static',
   /*
   ** Headers of the page
   */
   head: {
     title: process.env.npm_package_name || '',
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
