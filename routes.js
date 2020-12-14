c400cf9e20ead4f279f38967ce3ce0d52cf33481
aewshyae <1528058+aewshyae@users.noreply.github.com>
2020-12-15 07:29:34 +0900

    limit routes to show only 芥川龍之介

--- routes.js
+++ routes.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
 
+const AUTHOR_ID = 879 // 芥川龍之介
 export default async function routes () {
   try {
+    const bookIds = []
     const authors = await axios.get(process.env.NUXT_ENV_PERSON_DETAIL_URL).then((res) => {
-      return res.data.map((p) => {
+      return res.data.filter(p => p.id === AUTHOR_ID).map((p) => {
+        bookIds.push(...p.work.map(w => w.work_id))
         return {
           route: `/author/${p.id}`,
           payload: p
@@ -11,7 +14,7 @@ export default async function routes () {
       })
     })
     const books = await axios.get(process.env.NUXT_ENV_BOOK_CARD_URL).then((res) => {
-      return res.data.map((b) => {
+      return res.data.filter(b => bookIds.includes(b.title.work_id)).map((b) => {
         return {
           route: `/book/${b.title.work_id}`,
           payload: b
