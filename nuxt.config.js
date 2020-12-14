55a5572006bd46a4826a7155ef8d2654b84add8d
aewshyae <1528058+aewshyae@users.noreply.github.com>
2020-12-15 06:31:46 +0900

    add robots: nofollow,noindex

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'robots', content: 'nofollow,noindex'},
+      { name: 'robots', content: 'nofollow,noindex' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
